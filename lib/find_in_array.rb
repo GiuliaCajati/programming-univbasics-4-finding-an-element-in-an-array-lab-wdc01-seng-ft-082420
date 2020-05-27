adaad2ba51f6019a02093ae814fda9d0a17b41a5
Giulia Cajati <giuliacajati@gmail.com>
2020-05-27 15:45:52 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,9 @@
 def find_element_index(array, value_to_find)
-  count = 0 
+  count = 0
   while count < array.length do
-    puts array.index[count] == value_to_find 
-      found_value_index = count 
-  count += 1 
+    if array[count] == value_to_find
+      return count
+    end
+    count += 1
+  end
 end
